24ff1394adb4a6dbd257802c5f2d569d4e301327
kiu-art <kiusinh098@gmail.com>
2026-09-16 21:17:24 +0530

    feat: add SVG assets for React and Vite, create global CSS styles, and set up main entry point with ClerkProvider

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>My APP</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode='modal'/>
+          <SignUpButton mode='modal'/>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </div>
+  )
+}
+
+export default App
