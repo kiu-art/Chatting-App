8e14e6b6015da1367306915da24c9b7c971a8b5c
kiu-art <kiusinh098@gmail.com>
2026-09-17 20:13:38 +0530

    check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,5 +2,6 @@ export async function checkAuth(req,res,next){
     if(!req.user){
         return res.status(401).json({message:"Unauthorized"});
     }
+    console.log("Authorized")
     res.status(200).json(req.user)
 }
