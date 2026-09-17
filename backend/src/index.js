d26574273f1669926d8f7d2876e8981a2df7ef57
kiu-art <kiusinh098@gmail.com>
2026-09-17 19:56:10 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL
 
 const publicDir = path.join(process.cwd(),"public");
 
-app.use("/api/webhooks/clerk",express.raw({type:"/application/json"}),clerkWebhook);
+app.use("/api/webhooks/clerk",express.raw({type:"application/json"}),clerkWebhook);
 
 app.use(express.json());
 
