16a71ff206a220299faf0961200599218736ef6c
kiu-art <kiusinh098@gmail.com>
2026-09-20 14:43:59 +0530

    fix

--- backend/src/lib/imagekit.js
+++ backend/src/lib/imagekit.js
@@ -1,6 +1,6 @@
 import ImageKit, {toFile} from "@imagekit/nodejs";
 
-const imageKit = new ImageKit({privateKey:process.env.IMAGEKIT_PRIVATE_KEY});
+const imagekit = new ImageKit({privateKey:process.env.IMAGEKIT_PRIVATE_KEY});
 
 function hasIMageKitConfig(){
     return Boolean(process.env.IMAGEKIT_PRIVATE_KEY);
